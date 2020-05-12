55a204a6cbc56db8ea8a44b1200dd46c4be9c84f
cpt0202 <s2018w33@e-mirim.hs.kr>
2020-05-13 08:53:27 +0900

    admin QuestionAdmin fields

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -9,6 +9,7 @@ class ChoiceInline(admin.TabularInline):
 
 
 class QuestionAdmin(admin.ModelAdmin):
+
     fieldsets = [
         (None,               {'fields': ['question_text']}),
         ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
@@ -17,3 +18,5 @@ class QuestionAdmin(admin.ModelAdmin):
     list_display = ('question_text', 'pub_date')
 
 admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
+
